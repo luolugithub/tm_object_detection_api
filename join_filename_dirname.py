c3e0d6e97a2dd39cbe80576dbae7f76f83484ccb
tsukudamayo <tsukudamayo@gmail.com>
2019-02-03 12:32:07 +0900

    change join_filename_dirname.py to make sub directories 20180203

--- join_filename_dirname.py
+++ join_filename_dirname.py
@@ -1,9 +1,8 @@
 """
 preprocess filename not to duplicate filename
 """
-import os
 import argparse
-
+import os
 import shutil
 
 
@@ -12,35 +11,37 @@ def main(original_dir, target_dir):
         os.mkdir(os.path.dirname(target_dir))
     if os.path.exists(target_dir) is False:
         os.mkdir(target_dir)
-    
+
     for root, dirs, files in os.walk(original_dir):
         for f in files:
             org_filepath = os.path.join(root, f)
             filename = os.path.basename(org_filepath)
             dirname = os.path.basename(os.path.dirname(org_filepath))
-            # join dirname and filename not to duplicate filename
-            copy_filename =  dirname + '_' + filename
-            copy_filepath = os.path.join(target_dir, copy_filename)
+            childdir = os.path.join(target_dir, dirname)
+            if os.path.exists(childdir) is False:
+                os.mkdir(childdir)
+            copy_filename = dirname + '_' + filename
+            copy_filepath = os.path.join(childdir, copy_filename)
             # copy files to the target directory
             shutil.copy2(org_filepath, copy_filepath)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--original_dir',
-                                 dest='original_dir',
-                                 type=str,
-                                 default=None,
-                                 help='please input directory name'
-                                      'which you want to copy directories')
-    parser.add_argument('--target_dir',
-                                 dest='target_dir',
-                                 type=str,
-                                 default=None,
-                                 help='please input directory name'
-                                      'which you want to join'
-                                      'filename and dirname')
+    parser.add_argument(
+        '--original_dir',
+        dest='original_dir',
+        type=str,
+        default=None,
+        help='please input directory name'
+        'which you want to copy directories')
+    parser.add_argument(
+        '--target_dir',
+        dest='target_dir',
+        type=str,
+        default=None,
+        help='please input directory name'
+        'which you want to join'
+        'filename and dirname')
     argv = parser.parse_args()
     main(argv.original_dir, argv.target_dir)
-
-
