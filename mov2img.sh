049ef12cba05da8e00b0b9ddd5e82d82e87dfa0d
tsukuda.mayo <v_mayo.tukuda@pactera.com>
2019-02-08 17:50:46 +0900

    implement object_detection_video.py output classes scores
    object_detection_video.py output classes, scores
    requirements.txt add pandas scikit-learn
    add mov2img.sh
    add mov2img_crop.sh

---
+++ mov2img.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+_MOV_DIR=$1
+_TARGET_DIR=$2
+
+echo ${_MOV_DIR}
+echo ${_TARGET_DIR}
+mkdir ${_TARGET_DIR}
+
+count=0
+for f in `ls ${_MOV_DIR} | grep mp4`
+do
+    echo $count
+    echo $f
+    dir_name=$(echo $count | xargs -P1 printf '%06d\n')
+    echo $dir_name
+    mkdir ${_TARGET_DIR}'/'$dir_name
+
+    cd ${_TARGET_DIR}'/'$dir_name
+    ffmpeg -i ${_MOV_DIR}'/'$f -s 512x512 %06d.png
+    
+    count=$((count+1))
+done
--- object_detection_video.py
+++ object_detection_video.py
@@ -11,7 +11,7 @@ from object_detection.utils import visualization_utils as vis_util
 
 _PATH_TO_CKPT = 'export/export_20190206_157606/frozen_inference_graph.pb'
 _PATH_TO_LABELS = 'object_detection/data/gyoza_20190206_label_map.pbtxt'
-_NUM_CLASSES = 6
+_NUM_CLASSES = 7
 # _PATH_TO_CKPT = './ssd_mobilenet_v1_coco_2017_11_17/frozen_inference_graph.pb'
 # _PATH_TO_LABELS = 'object_detection/data/mscoco_label_map.pbtxt'
 # _NUM_CLASSES = 90
@@ -62,6 +62,10 @@ def main(start_offset, video_file):
             count = 0
             start_pos = fps * start_offset
             print('start_pos', start_pos)
+
+            classes_log = np.empty((0,100), int)
+            scores_log = np.empty((0,100), float)
+            
             # while (True):
             for i in range(start_pos, number_of_frames):
                 cap.set(cv2.CAP_PROP_POS_FRAMES, i)
@@ -86,31 +90,45 @@ def main(start_offset, video_file):
                 classes = detection_graph.get_tensor_by_name('detection_classes:0')
                 num_detection = detection_graph.get_tensor_by_name('num_detections:0')
 
-                if count % 5 == 0:
-                    (boxes, scores, classes, num_detection) = sess.run(
-                        [boxes, scores, classes, num_detection],
-                        feed_dict={image_tensor: _bbox},
-                    )
-                    print(
-                        vis_util.visualize_boxes_and_labels_on_image_array(
-                            bbox,
-                            np.squeeze(boxes),
-                            np.squeeze(classes).astype(np.int32),
-                            np.squeeze(scores),
-                            category_index,
-                            use_normalized_coordinates=True,
-                            line_thickness=20,
-                            max_boxes_to_draw=20,
-                        ))
-                    print(sys.stdout.write('classes : %s' % classes))
+                (boxes, scores, classes, num_detection) = sess.run(
+                    [boxes, scores, classes, num_detection],
+                    feed_dict={image_tensor: _bbox},
+                )
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                    bbox,
+                    np.squeeze(boxes),
+                    np.squeeze(classes).astype(np.int32),
+                    np.squeeze(scores),
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=20,
+                    max_boxes_to_draw=20,
+                )
+                print('classes : {}'.format(classes.astype(np.int32)))
+                print('a number of class : {} {}'.format(classes.shape[0], classes.shape[1]))
+                print('scores  : {}'.format(scores))
+                print('a number of score : {} {}'.format(scores.shape[0], scores.shape[1]))
 
+                print(sys.stdout.write('classes_log {}'.format(classes_log)))
+                print(sys.stdout.write('scores_log {}'.format(scores_log)))
+                classes_log = np.append(classes_log, np.array([classes[0].astype(np.int32)]), axis=0)
+                scores_log = np.append(scores_log, np.array([scores[0]]), axis=0)
+                    
                 cv2.imshow('frame', frame)
+                count += 1
+                print(count)
 
                 if cv2.waitKey(1) & 0xFF == ord('q'):
+                    # output log
+                    np.savetxt('classes.csv', classes_log, delimiter=',')
+                    np.savetxt('scores.csv', scores_log, delimiter=',')
                     break
 
             cap.release()
             cv2.destroyAllWindows()
+            # output log
+            np.savetxt('classes.csv', classes_log, delimiter=',')
+            np.savetxt('scores.csv', scores_log, delimiter=',')
 
 
 if __name__ == '__main__':
